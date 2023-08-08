bf6c694224b7f733e9edf2a6c7808736404b9635
Admin <v1rus3731@gmail.com>
2023-08-08 09:53:39 +0300

    commit changes

--- performers/window_performer.py
+++ performers/window_performer.py
@@ -11,7 +11,7 @@ class WindowPerformer():
     
     def show_window(self, roots: dict, data: dict = None, groups_pos: list = None):
         roots['root'].title(data['app_name'])
-        # roots['root'].iconbitmap(self._get_icon_path('network_folders_py/icon.ico'))
+        roots['root'].iconbitmap(self._get_icon_path('network_folders_py/icon.ico'))
         roots['root'].resizable(width=False, height=False)
         
         window_width = 500
