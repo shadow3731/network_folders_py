9c9b56849a57ad906279fdca99414c1bc52ac3b2
Admin <v1rus3731@gmail.com>
2023-08-14 14:03:44 +0300

    commit changes

--- cursor.py
+++ cursor.py
@@ -1,4 +1,14 @@
 class Cursor():
+    """The class for calculating objects placement on the window.
+    
+    Attributes:
+        x (int): X-coordinate of the object on the screen.
+        y (int): X-coordinate of the object on the screen.
+        width (int): The width of the object on the screen.
+        height (int): The height of the object on the screen.
+        padding (int): The distance between the total object area and the real one.
+        right_padding (int): The distance between the last object in the row and right edge of the window.
+        scr_width (int): The window width."""
     
     def __init__(
         self, 
@@ -8,7 +18,7 @@ class Cursor():
         height = 0, 
         padding = 0,
         right_padding = 0,
-        max_width = 0
+        scr_width = 0
     ):
         self.x = init_x
         self.y = init_y
@@ -16,9 +26,28 @@ class Cursor():
         self.height = height
         self.padding = padding
         self.right_padding = right_padding
-        self.screen_width = max_width
+        self.screen_width = scr_width
     
     def place_button(self, button: dict) -> tuple:
+        """Calculates a button positions.
+        
+        Positions of the button contain X- and Y-coordinate,
+        width and height. These values are saved in a tuple,
+        which is returned after calculation.
+        
+        If X-coordinate of the Cursor and width of the current button
+        are out of the width of the window, X- and Y-coordinate
+        of the Cursor transfer to new position.
+        
+        After calulating a button positions, Cursor changes 
+        its X-coordinate.
+        
+        Args:
+            button (dict): The appearance data of a button.
+            
+        Returns:
+            Positions (tuple) - of a button on the window."""
+        
         if self.x + button['size']*self.width > self.screen_width - self.padding - self.right_padding:
             self.x = self.padding
             self.y += self.height - self.padding
@@ -35,6 +64,21 @@ class Cursor():
         return button_positions
     
     def place_group(self, lower_y: int) -> tuple:
+        """Calculates a group positions.
+        
+        Positions of the group contain X- and Y-coordinate,
+        width and height. These values are saved in a tuple,
+        which is returned after calculation.
+        
+        After calulating a group positions, Cursor changes 
+        its Y-coordinate.
+        
+        Args:
+            lower_y (int): The lower Y-coordinate of the last button in this group.
+            
+        Returns:
+            Positions (tuple) - of a group on the window."""
+        
         group_positions: tuple = (
             self.x,
             self.y,
@@ -47,5 +91,7 @@ class Cursor():
         return group_positions
     
     def move_to_new_group(self):
+        """Moves Cursor to new position."""
+        
         self.x = self.padding
         self.y += self.height + 3*self.padding
--- dialog.py
+++ dialog.py
@@ -3,39 +3,40 @@ import os
 from tkinter import messagebox as mb
 from tkinter import filedialog as fd
 
-# import json
-
 class Dialog():
+    """The class for working with dialog windows."""
     
     def __init__(self):
         pass
     
     def show_error(self, message: str):
+        """Shows the 'askerror' dialog window with certain message.
+        
+        Args:
+            message (str): Error message to be shown."""
+        
         mb.showerror(
             title='Ошибка',
             message=message
         )
-        
-    # def save_file_dialog(self) -> str:
-    #     file = fd.asksaveasfile(
-    #         title='Создать файл визуализации',
-    #         defaultextension='.json',
-    #         filetypes=[('JSON файлы', '*.json'), ('Все файлы', '*.*')]
-    #     )
-    #     if file:
-    #         if file.name.endswith('.json'):
-    #             with open(file.name, 'w', encoding='utf8'):
-    #                 json.dump('{}', file)
-    #                 return file.name
-                
-    #         else:
-    #             message = "Файл визуализации должен иметь расширение .json."
-    #             self.show_error(message)
-    #             return None
-                
-    #     return None
     
     def open_file_dialog(self) -> str:
+        """Opens bult-in file dialog window 
+        to find a file with the appearance data.
+        
+        Only JSON-files are suitable to open, 
+        because it is supposed that the appearance data 
+        must be contained in this type of files.
+        
+        If a user opened file of another type, 
+        shows the 'askerror' window with error description.
+        
+        In case of safety, the default directory of file dialog 
+        is user's desktop.
+        
+        Returns:
+            File name (str) - of the JSON-file with the appearance data."""
+        
         file = fd.askopenfile(
             title='Открыть файл визуализации',
             initialdir=os.path.expanduser('~/Desktop'),
